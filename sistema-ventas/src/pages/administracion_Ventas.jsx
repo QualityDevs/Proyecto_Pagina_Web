9d7f58b02bee4cfc14660e53afbb21d5d955d882
sergio <seanviar@hotmail.com>
2021-10-02 10:34:47 -0500

    tablas venta y busqueda venta con estilos

--- sistema-ventas/src/pages/administracion_Ventas.jsx
+++ sistema-ventas/src/pages/administracion_Ventas.jsx
@@ -3,7 +3,123 @@ import React from 'react'
 const AdministracionVentas = () => {
     return (
         <div>
-            esta es la pagina de administracion_Ventas que edita natalia
+            
+            <form className="busqueda">
+
+        Buscar por:<select className='colorborde'>
+        <option>ID venta</option>
+        <option>Docuento cliente</option>
+        <option>Nombre cliente</option>
+        </select>
+
+        <input type="search" className='colorborde' />
+        <input type="submit" value="Buscar" className='colorborde'/>
+
+        
+        </form>
+  
+
+    <div className="tsergio">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID venta</th>
+                    <th>Fecha </th>
+                    <th>Unidades</th>
+                    <th>responsable</th>
+                    <th>Total venta</th>
+                    <th>Estado</th>
+                    <th>acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><a href="" className='mver'>Ver</a>     
+                        <a href="">Editar</a> </td>
+                </tr>
+                <tr className="sombra">
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr className="sombra">
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr className="sombra">
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td> 
+                    <td></td>
+                </tr>
+                <tr className="sombra">
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td> 
+                    <td></td>
+                </tr>
+
+            </tbody>
+        </table>
+    </div>
+            
         </div>
     )
 }
--- sistema-ventas/src/pages/registro_Ventas.jsx
+++ sistema-ventas/src/pages/registro_Ventas.jsx
@@ -1,9 +1,151 @@
 import React from 'react'
+import '../styles/styles-sergio.css'
 
 const RegistroVentas = () => {
     return (
         <div>
-            esta es la página de sergio para editar
+            <div className="tsergio estadoventa" >
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Estado de venta</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
+
+            <div className="tsergio">
+                <table>
+                    <thead>
+                        <tr>
+                        <th>ID Venta</th>
+                        <th>Fecha pedido</th>
+                        <th>Hora</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>0</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            
+            <div className="tsergio">
+                <table>
+                    <thead>
+                        <tr>
+                        <th>Cliente</th> 
+                        <th>Docuento de identidad</th> 
+                        <th>Responsable</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td>001</td>
+                            <td>1111111111</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <form className='fsergio'>
+
+                Agregar por:<select className='colorborde'>
+                <option >ID producto</option>
+                <option>Nombre producto</option>
+                </select>
+
+                <input type="search" className='colorborde' />
+                Unidades <input type="number" className='colorborde' />
+                <input type="submit" value="agregar" className='colorborde'/>
+
+                
+                </form>
+                
+            <div className="tsergio">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID productos</th>
+                            <th>Nombre producto</th>
+                            <th>Unidades</th>
+                            <th>Precio unidad</th>
+                            <th>Total unidades</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr className="sombra">
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr className="sombra">
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr className="sombra">
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <th className="total">total</th>
+                            <td>1111</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+
+           
+
+            <button type="submit" className="bsergio">Crear venta</button>
         </div>
     )
 }
