d5cac9b0e580cb980fc71f168e7907d07b5fd9c0
Laura Andrea Medina Sanchez <lauramedina630@gmail.com>
2021-10-01 23:30:35 -0500

    agregar admin_usuarios y diseño footer

--- sistema-ventas/src/Components/Footer.jsx
+++ sistema-ventas/src/Components/Footer.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import '../styles/stylesc.css';
+
 
 const Footer = () => {
     return (
         <footer>
-            Este es el footer
+    <div class="bg-light py-2">
+      <div class="container text-center">
+        <p class="text-muted mb-0 ">© 2021 <span>Zapatocas.</span> 2021 All rights reserved.</p>
+        <p class="text-muted mb-0 ">Designed by <span>QualityDevs</span></p>
+      </div>
+    </div>
         </footer>
     )
 }
--- sistema-ventas/src/pages/admin_Usuarios.jsx
+++ sistema-ventas/src/pages/admin_Usuarios.jsx
@@ -1,9 +1,148 @@
 import React from 'react';
+import '../styles/stylesc.css';
 
 const AdminUsuarios = () => {
     return (
-        <div>
-            esta es la pagina de admin_Usuarios
+        <div className="container"> 
+        <div className="item item-1">
+        <h2 >Usuarios</h2>
+        </div>
+            <div className="item item-2">
+            <form>
+            <table >
+            <thead>
+            <tr> 
+                <th>Id usuario</th> 
+                <th>Nombre</th> 
+                <th>Estado</th>
+                <th>Rol</th> 
+            </tr>
+            </thead>
+            <tbody>
+            <tr> 
+                <td>1234567899</td> 
+                <td>Abella Herrera William Efrain</td>
+                <td>
+                    <select>
+                        <option>Pendiente</option>
+                        <option>Autorizado</option>
+                        <option>No autorizado</option>
+                    </select>
+                </td>
+                <td>
+                    <select>
+                        <option>Vendedor</option>
+                        <option>Administrador</option>
+                    </select>
+                </td>
+            </tr>
+            <tr> 
+                <td>4963579855</td>
+                <td>Alegria Fernandez Diego Alonso</td>
+                <td>
+                <select>
+                        <option>Pendiente</option>
+                        <option>Autorizado</option>
+                        <option>No autorizado</option>
+                    </select>
+                </td>
+                <td>
+                    <select>
+                        <option>Vendedor</option>
+                        <option>Administrador</option>
+                    </select>
+                </td>
+            </tr>
+            <tr> 
+                <td>5785433474</td>
+                <td>Arenas De Escobar Maria Margoth</td>
+                <td>
+                <select>
+                        <option>Pendiente</option>
+                        <option>Autorizado</option>
+                        <option>No autorizado</option>
+                    </select>
+                </td>
+                <td>
+                    <select>
+                        <option>Vendedor</option>
+                        <option>Administrador</option>
+                    </select>
+                </td>
+            </tr>
+            <tr> 
+                <td>2497291443</td>
+                <td>Cabrera Leyton Hernando</td>
+                <td>
+                <select>
+                        <option>Pendiente</option>
+                        <option>Autorizado</option>
+                        <option>No autorizado</option>
+                    </select>
+                </td>
+                <td>
+                    <select>
+                        <option>Vendedor</option>
+                        <option>Administrador</option>
+                    </select>
+                </td>
+            </tr>
+            <tr> 
+                <td>1234567899</td>
+                <td>Egas Realpe Mauro Alberto</td>
+                <td>
+                <select>
+                        <option>Pendiente</option>
+                        <option>Autorizado</option>
+                        <option>No autorizado</option>
+                    </select>
+                </td>
+                <td>
+                    <select>
+                        <option>Vendedor</option>
+                        <option>Administrador</option>
+                    </select>
+                </td>
+            </tr>
+            <tr> 
+                <td>1474509792</td>
+                <td>Grijalba Gomez Pedro Felipe</td>
+                <td>
+                <select>
+                        <option>Pendiente</option>
+                        <option>Autorizado</option>
+                        <option>No autorizado</option>
+                    </select>
+                </td>
+                <td>
+                    <select>
+                        <option>Vendedor</option>
+                        <option>Administrador</option>
+                    </select>
+                </td>
+            </tr>
+            <tr> 
+                <td>1234567899</td>
+                <td>Hidalgo Otero Ana Liliana</td>
+                <td>
+                <select>
+                        <option>Pendiente</option>
+                        <option>Autorizado</option>
+                        <option>No autorizado</option>
+                    </select>
+                </td>
+                <td>
+                    <select>
+                        <option>Vendedor</option>
+                        <option>Administrador</option>
+                    </select>
+                </td>
+            </tr>
+            </tbody>
+            </table>
+            </form>
+            </div> 
+
         </div>
     );
 }
